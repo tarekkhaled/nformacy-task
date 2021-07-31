d7e02a96224b0098e38e3a0b125fc6c8edd1070c
tarekkhaled <tarekt820@gmail.com>
2021-08-01 01:11:52 +0200

    return articles status in serializer

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,7 +1,7 @@
 class ArticleSerializer
   include JSONAPI::Serializer
 
-  attributes :id, :title, :description
+  attributes :id, :title, :description, :status
 
   attribute :user, Proc.new { |record|
     record.user&.full_name
